d1249622175c4ddb561fd22c95f910c2debd0f2c
AKSHAT SRIVASTAVA <akshatsrivastava206@gmail.com>
2024-10-14 20:35:15 +0530

    added url for backend endpoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,7 @@ const Home = () => {
       try {
         setLoading(true); // Start loading when sending points
         const response = await axios.post(
-          "http://localhost:5000/extract-text",
+          "https://fdsbackend.onrender.com/extract-text",
           { points }, // Send points array as JSON
           {
             headers: { "Content-Type": "application/json" },
